7c5b28134f0ec16ba345cce904a5d80b4aef37b8
amdaza <amdazacastillo@gmail.com>
2016-06-06 21:46:00 +0200

    Configuring Single Page Application

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,9 @@
 // Module setter
 
-angular.module("cookbook", []);
+angular.module("cookbook", ["ngComponentRouter"]);
+
+// Configure provider $locationProvider.
+// Stablish navigation model HTML5 for Single Page Application to work
+angular.module("cookbook").config(function ($locationProvider) {
+    $locationProvider.html5Mode(true);
+});
