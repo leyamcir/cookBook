8ab066446479043c3e5fabdebe0685785204e931
leyamcir <leyamcir@gmail.com>
2016-06-08 16:19:14 +0200

    Hook list recipes $onInit

--- scripts/components/my-recipes.js
+++ scripts/components/my-recipes.js
@@ -2,29 +2,36 @@
 angular
     .module("cookbook")
     .component("myRecipes", {
-        // With 'template' / 'templateUrl' stablish compoment view
+        // With 'template' / 'templateUrl' establish component view
         templateUrl: "views/my-recipes.html",
 
-        // In 'controller', component logic is stablished
+        // In 'controller', component logic is established
         controller: function (recipesService) {
             // this references context, this context is the one we need, so we save it on self var
             var self = this;
 
-            recipesService.getRecipes()
-                .then(function (response) {
-                    // In 'data' property it's its body
-                    self.recipes = response.data;
-                });
+            // hook $onInit, fired when component is initialized
+            self.$onInit = function () {
+                recipesService.getRecipes() // Returns a promise
+                    .then(function (response) {
+                        // In 'data' property it's its body
+                        self.recipes = response.data;
+                    });
+            };
+            
+
         }
-        /*
-        * // This would be dependency injection
-        *
-        * controller: ["recipesService", function (recipesService) {
-        *     // ...
-        * }]
-        * */
     });
 
+
+
+/*
+ * // This would be dependency injection
+ *
+ * controller: ["recipesService", function (recipesService) {
+ *     // ...
+ * }]
+ * */
 
 /*
 * // This would be the code using injection
