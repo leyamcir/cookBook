4a338bc4672971cf2658da9e8e74f42b1156c09a
leyamcir <leyamcir@gmail.com>
2016-06-09 18:53:11 +0200

    Add Ingredients filter

---
+++ scripts/filters/ingredients-resume.js
@@ -0,0 +1,17 @@
+
+angular
+    .module("cookbook")
+    .filter("IngredientsResume", function () {
+        return function (ingredients) {
+            // Iterate ingredients collection to get from each a string,
+            // indicating ingredient (cant gr-)
+            var collection = ingredients.reduce(function (accumulated, ingredient) {
+                var text = ingredient.name + " (" + ingredient.quantity + " gr.)";
+                accumulated.push(text);
+                return accumulated;
+            },
+            []);
+
+            return collection.join(", ");
+        };
+    });
